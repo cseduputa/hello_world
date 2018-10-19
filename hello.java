a2243b7aa4017d5e6112e3bb72c891c0d61bf38d
cseduputa <43936152+cseduputa@users.noreply.github.com>
2018-10-19 07:30:51 -0400

    minor fix: program outputs new firstName + lastName values

--- hello.java
+++ hello.java
@@ -8,7 +8,7 @@ public class hello {
         String firstName = "Chisom";
         String lastName = "Sunny-Eduputa";
 	
-        System.out.println("Hello Class!");
+        System.out.println("Hello, my name is " + firstName + " " + lastName + ".");
 
 
 
